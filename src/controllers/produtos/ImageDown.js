08f8cca7c2833e934f1d47caf7f9bcac83d82929
rafinhahdc19 <rafinhacarvalhorlc@gmail.com>
2024-03-03 23:05:41 -0300

    First commit

--- src/controllers/produtos/ImageDown.js
+++ src/controllers/produtos/ImageDown.js
@@ -9,7 +9,7 @@ const ImageDown = async (req, res) => {
   try {
     const parsedUrl = new URL(imageUrl);
 
-    if (parsedUrl.hostname === 'photo.yupoo.com') {
+    if (parsedUrl.hostname === 'photo.yupoo.com' || parsedUrl.hostname === 'storage.googleapis.com') {
       const options = {
         headers: {
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36',
